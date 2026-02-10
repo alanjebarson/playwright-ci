fe01dfcf960100ec3ca9842ac655882ffbb1142d
Alan Jebarson <alanjebarson@gmail.com>
2026-02-10 21:58:40 +0530

    Added New URL

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,18 +2,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://demo.automationtesting.in/Static.html');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Drag and Drop/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('click home link', async ({ page }) => {
+  await page.goto('https://demo.automationtesting.in/Static.html');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Home' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects page to have a text with the name of Email id or Sign Up.
+  await expect(page.getByPlaceholder('Email id for Sign Up')).toBeVisible();
 });
